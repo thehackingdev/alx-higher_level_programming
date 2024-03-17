cb37a35ea3c5239120157b5d74fa9ba139b7c624
Nathan Avaaga <nathanavaaga@gmail.com>
2024-03-17 22:54:53 +0300

    data structure

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * palindrome - is_palindrome
+ * @top: points to a pointer to singly linked list
+ * @next: pointer to a singly linked list
+ *
+ * Return: integer, 1 if palindrome, else 0
+ */
+int palindrome(listint_t **top, listint_t *next)
+{
+	int result = 0;
+
+	if (next == NULL)
+		return (1);
+
+	if (palindrome(top, next->next) && ((*top)->n == next->n))
+		result = 1;
+
+	*top = (*top)->next;
+
+	return (result);
+}
+
+/**
+ * is_palindrome - check whether singly linked list is a palindrome
+ * @head: linked list double pointer
+ *
+ * Return: integer, 1 if list is a palindrome else 0
+ */
+int is_palindrome(listint_t **head)
+{
+	return (palindrome(head, *head));
+}
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new = ''
+    for i in range(len(my_string)):
+        if my_string[i].lower() != 'c':
+            new += my_string[i]
+    return new
