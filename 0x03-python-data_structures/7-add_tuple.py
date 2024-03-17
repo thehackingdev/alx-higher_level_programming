88ec96f4a0d3b39a8324eaea764ae310203b0064
Nathan Avaaga <nathanavaaga@gmail.com>
2024-03-17 22:59:14 +0300

    data structure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    ta = resolve(tuple_a)
-    tb = resolve(tuple_b)
-    return (ta[0] + tb[0], ta[1] + tb[1])
+    if (len(tuple_a) == 0):
+        tuple_a = (0, 0)
+    elif (len(tuple_a) == 1):
+        tuple_a = (tuple_a[0], 0)
+    if (len(tuple_b) == 0):
+        tuple_b = (0, 0)
+    elif (len(tuple_b) == 1):
+        tuple_b = (tuple_b[0], 0)
 
-def resolve(t=()):
-    tlen = len(t)
-    if tlen == 0:
-        tnew = 0, 0
-    elif tlen == 1:
-        tnew = t[0], 0
-    else:
-        tnew = t[0], t[1]
-    return tnew
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
