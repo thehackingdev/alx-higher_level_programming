ca85f5bf4b060845110188e12fdcba1328eaeeea
Nathan Avaaga <nathanavaaga@gmail.com>
2024-03-20 13:04:04 +0300

    more data structures

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# Python program that demonstrates how to return a list
-
-
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda i: number * i, my_list))
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda element: element * number, my_list)))
