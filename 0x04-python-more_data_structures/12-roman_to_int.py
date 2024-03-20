97beeb9802477e63d28eb6e26e516f94676e20a5
Nathan Avaaga <nathanavaaga@gmail.com>
2024-03-20 13:27:21 +0300

    more data structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,57 @@
 #!/usr/bin/python3
 
+def value(roman):
+    """returns value of each Roman symbol
+    """
+    if roman == 'I':
+        return 1
+    if roman == 'V':
+        return 5
+    if roman == 'X':
+        return 10
+    if roman == 'L':
+        return 50
+    if roman == 'C':
+        return 100
+    if roman == 'D':
+        return 500
+    if roman == 'M':
+        return 1000
+    return 0
 
 
 def roman_to_int(roman_string):
+    """converts a Roman numeral to an integer
+    def roman_to_int(roman_string) must return an integer
+    If the roman_string is not a string or None, return 0
+    """
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
     result = 0
-    if type(roman_string) == str and roman_string is not None:
-        string = roman_string
-        strlen = len(string)
-        std = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        for i in range(strlen):
-            if (i > 0) and (std[string[i]] > std[string[i - 1]]):
-                result += int(std[string[i]]) - int(std[string[i - 1]]) * 2
+    i = 0
+
+    while i < len(roman_string):
+        # getting value of symbol roman_string[i]
+        str1 = value(roman_string[i])
+
+        if i + 1 < len(roman_string):
+            # getting value of symbol roman_string[i + 1]
+            str2 = value(roman_string[i + 1])
+
+            # comparing both values
+            if str1 >= str2:
+                # Value of current symbol is greater or equal
+                result = result + str1
+                i = i + 1
             else:
-                result += std[string[i]]
+                # Value of current symbol is greater or equal to the next
+                result = result + str2 - str1
+                i = i + 2
+        else:
+            result = result + str1
+            i = i + 1
     return result
