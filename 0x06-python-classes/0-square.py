eadabf5b95409c089a5812acbfb3a6b1db18c083
Nathan Avaaga <nathanavaaga@gmail.com>
2024-04-02 11:37:58 +0300

    python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" defines a empty class square """
+
+
+class Square:
+    """ empty class for task 0 """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,4 @@
+0x02. Python - Classes
+This is an ALX School project on classes on python.
+
+
