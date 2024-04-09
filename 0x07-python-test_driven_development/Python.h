9f44423615b00730a37beadd00831e8eacfd88bf
Nathan Avaaga <nathanavaaga@gmail.com>
2024-04-10 00:19:44 +0300

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,9 @@
+#ifndef PYTHON_STRING_H
+#define PYTHON_STRING_H
+
+#include <stdio.h>  // For standard input/output functions (printf)
+
+// Function prototype for print_python_string
+void print_python_string(PyObject *p);
+
+#endif  // PYTHON_STRING_H
